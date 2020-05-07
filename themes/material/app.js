3c2cde4e3c2e64e95d0513128422dce1954ae08a
Brief-rf <57308180+Brief-rf@users.noreply.github.com>
2020-05-07 16:52:16 +0800

    Update app.js

--- themes/material/app.js
+++ themes/material/app.js
@@ -293,6 +293,22 @@ function file_video(path){
 	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
 	</div>
 </div>
+<div class="mdui-container-fluid">
+	<br>
+	<video class="mdui-video-fluid mdui-center" preload controls>
+	  <source src="${url}" type="video/mp4">
+	</video>
+	<br>${playBtn}
+	<!-Fixed label->
+	<div class="mdui-textfield">
+	  <label class="mdui-textfield-label">download link</label>
+	  <input class="mdui-textfield-input" type="text" value="${url}"/>
+	</div>
+	<div class="mdui-textfield">
+	  <label class="mdui-textfield-label">HTML reference</label>
+	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
+	</div>
+</div>
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
 	$('#content').html(content);
